2e7c9f25ac09f35d37eb98580954f147f4f2c3c6
AR-92 <electro.electra@gmail.com>
2025-04-03 08:03:13 +0500

    Add initial TailwindCSS config and styles
    This commit includes:
    - The addition of a `flowAi.css` file containing TailwindCSS
      configurations and utility classes.
    - Updates to `dev.sh` to run the TailwindCSS CLI.
    - Adjustments to `index.html` to use `flowAi.css`.
    - Minor updates to `output.css`.
    - The addition of a modal to `sideBar.html`.

--- dev.sh
+++ dev.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
+# Kill any existing server process on port 3000 (only if it exists)
+pid=$(sudo lsof -t -i:3000)
+if [[ -n "$pid" ]]; then
+  sudo kill -9 "$pid"
+fi
+
+# Kill any existing TailwindCSS process (only if it exists)
+pid_tailwind=$(pgrep -f "tailwindcss")
+if [[ -n "$pid_tailwind" ]]; then
+  sudo kill -9 "$pid_tailwind"
+fi
+
 # Start the Node-Server
 node server &
 
-# Start the standalone CLI (make sure it's available in your PATH)
-tailwindcss -i ./assets/css/input.css -o ./assets/css/output.css --watch &
+# Start the standalone TailwindCSS CLI
+tailwindcss -i ./assets/css/input.css -o ./assets/css/flowAi.css --watch & disown &
 
-# Wait for both background processes to finish (this will keep the script running)
+# Wait for both background processes to finish
 wait
 
 # instructions on how to use the script
